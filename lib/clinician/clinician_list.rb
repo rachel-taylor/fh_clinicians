f50dc8c20f20a7406512014b694258842ec8c4ac
Rachel T <rachmaybe@gmail.com>
2019-10-20 23:25:36 +0000

    Updated clinicianlist

--- lib/clinician/clinician_list.rb
+++ lib/clinician/clinician_list.rb
@@ -1,1 +1,17 @@
-class fh_clinicians:
+class fh_clinicians::clinician_list
+  attr_accessor :name, :phone, :email 
+  
+  @@all = []
+  
+  def initialize(name,phone,email)
+    @name = name
+    @phone = phone
+    @email = email 
+    @@ all << self 
+  end 
+  
+  def self.all
+    @@all 
+  end 
+  
+end 
