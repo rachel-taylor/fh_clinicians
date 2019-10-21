fa505c0cd29b7b9bd616565bf893c40e276ef976
Rachel T <rachmaybe@gmail.com>
2019-10-21 22:47:45 +0000

    Updated Scraper

--- lib/clinician/scrape.rb
+++ lib/clinician/scrape.rb
@@ -10,7 +10,7 @@
    def self.scrape
      doc = Nokogiri::HTML(open("https://www.fredhutch.org/en/research/patient-treatment-support/meet-our-clinicians.html"))
      puts doc 
-     specialty = doc.css(".faq-title").each do |anchor|
+     specialty = doc.css("h2").each do |anchor|
       name = anchor.css("h3").text
         puts name 
       phone_number = anchor.css("contact-card--phone").text
