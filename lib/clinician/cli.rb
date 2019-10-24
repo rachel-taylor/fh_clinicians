008e45668e61816bf256931a9c31438e24779cd7
Rachel T <rachmaybe@gmail.com>
2019-10-24 03:30:55 +0000

    Updated CLI

--- lib/clinician/cli.rb
+++ lib/clinician/cli.rb
@@ -5,15 +5,20 @@ require './lib/clinician/specialty'
 class Clinician::CLI 
   
   def welcome
-    puts "Welcome to the Fred Hutchinson Clinician tool, which specialty are you seeking?"
-    puts specialty
+    puts "Welcome to Fred Hutchinson, here are our specialties"
+     i=0 
+    specialty.each do |thething| 
+      i += 1
+      puts i.to_s + "-" + thething 
+      # puts i
+    end 
   end 
   
   def specialty
   #    scraper = Clinician::SCRAPE.new()
     # scraper.scrape  
     Clinician::SCRAPE.scrape 
-    Clinician::SCRAPE.specialty
+    # Clinician::SCRAPE.specialty
   end 
   
   
--- lib/clinician/scrape.rb
+++ lib/clinician/scrape.rb
@@ -11,7 +11,7 @@
     
    def self.scrape
      doc = Nokogiri::HTML(open("https://www.fredhutch.org/en/research/patient-treatment-support/meet-our-clinicians.html"))
-      @@specialty = doc.css("div.faq-title h2").text do |anchor|
+      @@specialty = doc.css("div.faq-title h2").map do |anchor|
        specialty = anchor.text
       end 
   end 
